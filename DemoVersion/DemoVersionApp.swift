f3546be32416564f6421213ad899b7b3c4aec6af
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2025-12-16 21:52:37 +0500

    Initial Commit

---
+++ DemoVersion/DemoVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVersionApp.swift
+//  DemoVersion
+//
+//  Created by Nadira Seitkazy  on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoVersionTests/DemoVersionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVersionTests.swift
+//  DemoVersionTests
+//
+//  Created by Nadira Seitkazy  on 16.12.2025.
+//
+
+import Testing
+@testable import DemoVersion
+
+struct DemoVersionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
